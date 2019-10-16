8b626b53dd7b9fc8e97a96fbc26f23844cf74b94
Pinkdesu <52241746+Pinkdesu@users.noreply.github.com>
2019-10-16 23:47:49 +0500

    main part3

---
+++ src/components/filter-item/filter-item.jsx
@@ -0,0 +1,16 @@
+import React, { Component, useState }  from 'react';
+import './filter-item-style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+export default () => {
+    const [isCollapsed, handleClick] = useState(true);
+
+    return(
+        <div className="filter-item-bar" onClick={() => handleClick(isCollapsed)}>
+            <div className="filter-item-head">
+                <FontAwesomeIcon icon={isCollapsed ? faAngleDown : faAngleUp }/>
+            </div>
+        </div>
+    );
+}
