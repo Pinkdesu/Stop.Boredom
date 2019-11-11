2b88bf657dacd4bac2ac8f0636740632d716a4b7
Pinkdesu <52241746+Pinkdesu@users.noreply.github.com>
2019-11-11 22:50:34 +0500

    animation fix

--- src/components/answer-popup/answer-popup.jsx
+++ src/components/answer-popup/answer-popup.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import AnswerPopupLiElement from '../answer-popup-li-element/answer-popup-li-element';
 import './answer-popup-style.scss'
 
 const AnswerPopup = ( {isDeployedAnswerWindow} ) => {
     return(
-        <div className={isDeployedAnswerWindow ? "answer-popup-wrap" : "answer-popup-wrap collapsed"}>
-            <span>Результат</span>
-            <ul>
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-                <AnswerPopupLiElement />
-            </ul>
-        </div>
+        <Fade duration={500} when={isDeployedAnswerWindow}>
+            <div className={isDeployedAnswerWindow ? "answer-popup-wrap" : "answer-popup-wrap collapsed"}>
+                <span>Результат</span>
+                <ul>
+                    <AnswerPopupLiElement />
+                    <AnswerPopupLiElement />
+                    <AnswerPopupLiElement />
+                    <AnswerPopupLiElement />
+                    <AnswerPopupLiElement />
+                    <AnswerPopupLiElement />
+                </ul>
+            </div>
+        </Fade>
     );
 }
 
--- src/components/filters-list/filters-list.jsx
+++ src/components/filters-list/filters-list.jsx
@@ -1,71 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { removeActiveFilters } from '../../actions/actionCreator';
+import Fade from 'react-reveal/Fade';
 import FilterLiElement from '../filter-li-element/filter-li-element';
 import './filters-list-style.scss';
 
-// const FiltersList = ({ sectionID, isDeployed, filters, removeActiveFilters, activeFilters }) => {
+const FiltersList = ({ sectionID, isDeployed, filters, removeActiveFilters, activeFilters }) => {
 
-//     const [count, SetCount] = useState(0);
+    const [count, SetCount] = React.useState(0);
 
-//     const getActiveFiltersCount = (value) => {
-//         SetCount(count + value);
-//         //  let count = activeFilters.reduce( (sum, current) =>  {
-//         //      let filtersIDs = filters.map(filter => filter.id);
-//         //      return filtersIDs.includes(current.id) ? ++sum : sum;
-//         // }, 0);
-
-//         // let count = 0;
-//         // if(document.getElementById(sectionID))
-//         // {
-//         //     count = filters.reduce((sum, current) => {
-//         //         return document.getElementById(current.id).checked ? ++sum : sum;
-//         //     }, 0)
-//         // }
-//         // return count;
-//     }
-
-//     //console.log("render"); слишком много переренд. компонента, переделать
-//     return(
-//         <div className={isDeployed ? "filters-list-wrap" : "filters-list-wrap collapsed"}>
-//             <div className="filters-list-header">
-//                 <button onClick={() => removeActiveFilters(filters.map(filter => filter.id))}>Очистить</button>
-//                 <span>Выбрано: {count}</span>
-//             </div>
-//             <ul id={sectionID} className="ul-list">
-//                 {filters.map(({ id, text }) => (
-//                     <FilterLiElement 
-//                         key={id} 
-//                         id={id}
-//                         text={text}
-//                         getActiveFiltersCount={() => getActiveFiltersCount()}/>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// }
-class FiltersList extends Component{
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0,
-        };
-        
-        this.setActiveFiltersCount = this.setActiveFiltersCount.bind(this);
+    const setActiveFiltersCount = (value) => {
+        SetCount(count + +value);
     }
 
-    setActiveFiltersCount = (value) => {
-        this.setState(state => ({
-            count: state.count + value
-        }));
-    }
-
-    render(){
-        const { sectionID, isDeployed, filters, removeActiveFilters } = this.props;
-        const { count } = this.state;
-        return(
-            <div className={isDeployed ? "filters-list-wrap" : "filters-list-wrap collapsed"}>
+    return(
+        <div className={isDeployed ? "filters-list-wrap" : "filters-list-wrap collapsed"}>
+            <Fade when={isDeployed} delay={100} duration={300}>
                 <div className="filters-list-header">
                     <button onClick={() => removeActiveFilters(filters.map(filter => filter.id))}>Очистить</button>
                     <span>Выбрано: {count}</span>
@@ -76,12 +26,12 @@ class FiltersList extends Component{
                             key={id} 
                             id={id}
                             text={text}
-                            setActiveFiltersCount={this.setActiveFiltersCount}/>
+                            setActiveFiltersCount={(value) => setActiveFiltersCount(value)}/>
                     ))}
                 </ul>
-            </div>
-        );
-    }
+            </Fade>
+        </div>
+    );
 }
 
 export default connect(() => ({}), { removeActiveFilters })(FiltersList);
