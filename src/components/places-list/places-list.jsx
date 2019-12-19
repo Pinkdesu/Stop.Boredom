afda25b806639bf70132406d9b1e3bf4de9cb3c6
Pinkdesu <52241746+Pinkdesu@users.noreply.github.com>
2019-12-19 19:17:25 +0500

    places-list logic

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./places-list-style.scss";
 
 const PlacesList = () => {
-  const [isDeployed, SetDeployed] = useState(false);
+  const [isDeployed, setDeployed] = useState(false);
+
+  const handleButtonClick = () => {
+    setDeployed(!isDeployed);
+  };
 
   return (
     <div className="places-list-wrapper">
-      <h2>Кафе</h2>
+      <h1>Кафе</h1>
+      <div className="places-list-main"></div>
+      <div classNmae="places-list-footer">
+        <button onClick={handleButtonClick}>
+          <span>{isDeployed ? "Свернуть" : "Развернуть"}</span>
+          <FontAwesomeIcon icon={isDeployed ? faAngleUp : faAngleDown} />
+        </button>
+      </div>
     </div>
   );
 };
