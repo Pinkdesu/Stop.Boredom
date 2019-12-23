430d4430f5aed161238cd2b16905d4b1c7490beb
Pinkdesu <52241746+Pinkdesu@users.noreply.github.com>
2019-12-23 11:41:53 +0500

    add new font style

--- src/components/carousel-item/carousel-item.jsx
+++ src/components/carousel-item/carousel-item.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { CATEGORY_ICONS } from "../../constants";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./carousel-item-style.scss";
 
 const CarouselItem = ({ categories }) => {
   return (
     <div className="carousel-item">
-      {categories.map(({ id, name }) => (
-        <div key={id}>
+      {CATEGORY_ICONS.map(({ id, name, icon }) => (
+        <Link to={`/places/${id}`} key={id}>
+          <FontAwesomeIcon icon={icon} size="2x" />
           <h1>{name}</h1>
-          <p>{id}</p>
-        </div>
+        </Link>
       ))}
     </div>
   );
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,11 @@
+import {
+  faTree,
+  faHamburger,
+  faTape,
+  faWalking
+} from "@fortawesome/free-solid-svg-icons";
+//import { faTree } from "@fortawesome/free-regular-svg-icons";
+
 export const ADD_ALL_FILTERS = "ADD_ALL_FILTERS";
 export const ADD_ACTIVE_FILTER = "ADD_ACTIVE_FILTER";
 export const REMOVE_ACTIVE_FILTER = "REMOVE_ACTIVE_FILTER";
@@ -5,6 +13,32 @@ export const REMOVE_ACTIVE_FILTERS = "REMOVE_ACTIVE_FILTERS";
 export const ADD_ALL_PLACES = "ADD_ALL_PLACES";
 export const ADD_ALL_CATEGORIES = "ADD_ALL_CATEGORIES";
 export const LOGIN_REQUEST = "LOGIN_REQUEST";
+
+export const CATEGORY_ICONS = [
+  {
+    id: 1,
+    name: "Парки",
+    icon: faTree
+  },
+
+  {
+    id: 2,
+    name: "Поесть",
+    icon: faHamburger
+  },
+
+  {
+    id: 3,
+    name: "Кино",
+    icon: faTape
+  },
+
+  {
+    id: 4,
+    name: "Погулять",
+    icon: faWalking
+  }
+];
 
 export const FORM_TEXT = [
   {
